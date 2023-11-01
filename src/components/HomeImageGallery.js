99799713ff89aff2a23654271709d0cd6ddfa39c
rifatjahan823 <rifatjahan823@gmail.com>
2023-11-01 22:43:57 +0600

    add delete

--- src/components/HomeImageGallery.js
+++ src/components/HomeImageGallery.js
@@ -5,6 +5,41 @@ import UploadImg from "../utils/api/UploadImg";
 
 const HomeImageGallery = () => {
   const [images, setImages] = useState([]);
+  const [selectedImageIds, setSelectedImageIds] = useState([]);
+
+  const handleImageSelection = (imageId) => {
+    if (selectedImageIds.includes(imageId)) {
+      setSelectedImageIds(selectedImageIds.filter((id) => id !== imageId));
+    } else {
+      setSelectedImageIds([...selectedImageIds, imageId]);
+    }
+  };
+
+  const handleDeleteSelectedImages = () => {
+    // Create an array of selected image IDs
+    const selectedImageIdsArray = selectedImageIds.map((id) => ({ id }));
+
+    // Send a DELETE request to your API to delete the selected images using the selectedImageIds array.
+    fetch('http://localhost:5000/delete-selected-images', {
+      method: 'DELETE',
+      body: JSON.stringify({ selectedImageIds: selectedImageIdsArray }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Handle the success case, and then update the images state to reflect the changes.
+          // For example, fetch the updated image data and set it using setImages.
+        } else {
+          console.error('Failed to delete selected images.');
+        }
+      })
+      .catch((error) => {
+        console.error('Error deleting selected images:', error);
+      });
+  };
+
 
   useEffect(() => {
     fetch("http://localhost:5000/image")
@@ -32,7 +67,9 @@ const HomeImageGallery = () => {
   };
 
   return (
-    <div className="grid-wrapper">
+   <div>
+      <button onClick={handleDeleteSelectedImages}>Delete Selected</button>
+     <div className="grid-wrapper">
       {images.map((img, index) => (
         <div
           key={index}
@@ -42,11 +79,13 @@ const HomeImageGallery = () => {
           onDrop={(event) => onDrop(event, index)}
           className="image-container"
         >
-          <GetImg img={img} />
+          <GetImg img={img} isSelected={selectedImageIds.includes(img._id)}
+            onImageSelection={handleImageSelection}/>
         </div>
       ))}
       <UploadImg />
     </div>
+   </div>
   );
 };
 
--- src/utils/api/GetImg.js
+++ src/utils/api/GetImg.js
@@ -1,45 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../styles/HomeImageGallery.css'
 
 
-const GetImg = ({ img }) => {
-    const { picture } = img;
-  
+const GetImg = ({ img, isSelected, onImageSelection }) => {
+    const { picture,_id } = img;
+    
   // Use state to track the checkbox's visibility
-  const [showCheckbox, setShowCheckbox] = useState(false);
+  // const [showCheckbox, setShowCheckbox] = useState(false);
 
   // Use state to track the checkbox's checked state
-  const [isChecked, setIsChecked] = useState(false);
+  // const [isChecked, setIsChecked] = useState(false);
 
-  const handleMouseEnter = () => {
-    setShowCheckbox(true);
-  };
+  // const handleMouseEnter = () => {
+  //   setShowCheckbox(true);
+  // };
 
-  const handleMouseLeave = () => {
-    if (!isChecked) {
-      setShowCheckbox(false);
-    }
-  };
+  // const handleMouseLeave = () => {
+  //   if (!isChecked) {
+  //     setShowCheckbox(false);
+  //   }
+  // };
 
-  const handleCheckboxClick = () => {
-    setIsChecked(!isChecked);
-  };
+  // const handleCheckboxClick = () => {
+  //   setIsChecked(!isChecked);
+  // };
+
+
+  //handle delete by checkbox
+ 
   
     return (
-      <div
-        className="Getimage-container"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        <img src={picture} alt="" />
-  
-        {/* Conditional rendering of the checkbox */}
-      {showCheckbox && (
-        <div className="checkbox" onClick={handleCheckboxClick}>
-          <input type="checkbox" checked={isChecked} />
-        </div>
-      )}
+      <div className="Getimage-container" onClick={() => onImageSelection(_id)}>
+      <img src={picture} alt="" />
+
+      <div className="checkbox">
+        <input type="checkbox" checked={isSelected} />
       </div>
+    </div>
     );
   };
 export default GetImg;
